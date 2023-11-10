44ccd12d8a08588b9434334aafa163dc2eea32c9
Bruno Souza <brunoezully@gmail.com>
2023-11-09 22:17:40 -0300

    feat: Room component to Home page

---
+++ src/components/Room.tsx
@@ -0,0 +1,14 @@
+
+
+export default function Room() {
+  return (
+    <div className="flex">
+      <div className="bg-zinc-800 w-96 h-screen overflow-auto ">
+
+      </div>
+      <div className="flex flex-col flex-1 ">
+
+      </div>
+    </div>
+  )
+}
